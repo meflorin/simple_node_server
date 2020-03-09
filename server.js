5e988ebc7ed2a86394d621a84d9de2e461deef2a
meflorin <50907790+meflorin@users.noreply.github.com>
2020-03-09 20:43:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+var Schema = mongoose.Schema;
 
+mongoose.set('debug',true);
 const app = express();
 
 app.use(cors());
@@ -10,13 +12,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+// Connect to MongoDB
+mongoose
+    .connect( 
+        //'mongodb+srv://ivanov1502:mikelancelo1.@cluster0-q271w.mongodb.net/test?retryWrites=true&w=majority', 
+        'mongodb+srv://ivanov1502:mikelancelo1.@cluster0-q271w.mongodb.net/Ivanov?retryWrites=true&w=majority', 
+      { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+    
+//define mongoose schema
+var schemaName = new Schema({
+    title: String
+}, {
+    collection: 'ivanov'
+});
 
-    //get data from db 
-    //.........
+var IvanovModel = mongoose.model('IvanovModel', schemaName);
 
+app.get("/", (req, res) => {  
+    //get data from db
 
-    res.send("Hello world");
+    IvanovModel.find({
+    }, function(err, result) {
+        if (err) throw err;
+        if (result) {
+            //console.log(result);
+            res.json(result)
+        } else {
+            res.send(JSON.stringify({
+                error : 'Error'
+            }));
+        }
+    });
   });
 
 const PORT = 7777;
